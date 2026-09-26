ea0cb5c319b4c58ade619cc2f2f42f2e160d09fd
Rimru01 <136918667+Rimru01@users.noreply.github.com>
2026-09-26 20:38:25 +0300

    multimedia: update pipewire and wireplumber packages

--- multimedia/video/wireplumber/actions.py
+++ multimedia/video/wireplumber/actions.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
-# Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
+
 
 def setup():
-    #shelltools.system('sed -i "/doxygen.sh/d" "spice-0.15.0/meson.build"')
-    mesontools.configure()
+    mesontools.configure(
+        "--buildtype=plain "
+        "-D systemd=disabled "
+        "-D systemd-system-service=false "
+        "-D systemd-user-service=false "
+        "-D elogind=disabled "
+        "-D system-lua=true"
+    )
+
 
 def build():
     mesontools.build()
 
+
 def check():
     mesontools.build("test")
 
+
 def install():
-    mesontools.install()
 
     #shelltools.copytree("%s/usr/share/pkgconfig" % get.installDIR(), "%s/usr/lib/pkgconfig" % get.installDIR())
     #pisitools.removeDir("/usr/share/pkgconfig")
-    pisitools.dodoc("LICENSE", "README.rst")
+    mesontools.install()
+    pisitools.dodoc("LICENSE", "README*")
